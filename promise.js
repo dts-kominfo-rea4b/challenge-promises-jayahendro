cca9d8468cae1287dad038d888b6d1a6d1ac6f31
jayahendro <jaya.hendra05@gmail.com>
2022-09-21 12:00:03 +0700

    [DONE] Challenge Promises

--- promise.js
+++ promise.js
@@ -1,7 +1,10 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = (value) => {
+  if (value === 'marah') return 4
+  else if (value === 'tidak marah') return 2
+};
 
 module.exports = {
   promiseOutput,
