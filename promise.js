4e3d140cf92d00f79c8411dd56341ab7728f0d45
jayahendro <jaya.hendra05@gmail.com>
2022-09-22 17:21:05 +0700

    [UPDATE] finish

--- promise.js
+++ promise.js
@@ -1,9 +1,21 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = (value) => {
-  if (value === 'marah') return 4
-  else if (value === 'tidak marah') return 2
+const promiseOutput = async (value) => {
+  let count = 0
+  let value1, value2
+  value1 = await promiseTheaterIXX()
+  value2 = await promiseTheaterVGC()
+
+  const data = [...value1, ...value2]
+
+  data.map((result) => {
+    if (result.hasil === value) {
+      count++
+    }
+  })
+
+  return count
 };
 
 module.exports = {
